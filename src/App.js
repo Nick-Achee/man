418d9bc13a779df5597506d2f35227fcc26989e2
Nick Achee <64217718+Nick-Achee@users.noreply.github.com>
2022-03-21 09:53:52 -0700

    change postions and animation speed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Suspense, useRef, useState, useEffect } from "react"
-import { Canvas, useFrame } from "react-three-fiber"
+import { Canvas, useFrame, useThree } from "react-three-fiber"
 import { ContactShadows, Environment, useGLTF, OrbitControls, Html, useProgress } from "drei"
 import { proxy, useProxy } from "valtio"
 import { Button, Grid } from '@mui/material';
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
+const deg2rad = degrees => degrees * (Math.PI / 180);
 
 
 
@@ -31,21 +32,25 @@ const state = proxy({
 })
 
 function Shoe(props) {
+  
   const ref = useRef()
   const snap = useProxy(state)
   // Drei's useGLTF hook sets up draco automatically, that's how it differs from useLoader(GLTFLoader, url)
   // { nodes, materials } are extras that come from useLoader, these do not exist in threejs/GLTFLoader
   // nodes is a named collection of meshes, materials a named collection of materials
   const { nodes, materials } = useGLTF("/untitled.glb")
+  
+
 
   // Animate model
   useFrame((state) => {
     const t = state.clock.getElapsedTime()
     ref.current.rotation.z = 0.02 - (1 + Math.sin(t / 1.5)) / 50
     ref.current.rotation.x = Math.sin(t / 4) / 15
-    ref.current.rotation.y = Math.cos(t / -69) / .019
+    ref.current.rotation.y = Math.cos(t / -100) / .003
     ref.current.position.y = Math.sin(t / 1) / 20
   })
+
 
   // Cursor showing current color
   const [hovered, set] = useState(null)
@@ -53,7 +58,10 @@ function Shoe(props) {
     const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`
     const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`
     document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(hovered ? cursor : auto)}'), auto`
-  }, [hovered])
+  }, [hovered]);
+  
+
+  
 
   // Using the GLTFJSX output here to wire in app-state and hook up events
   return (
@@ -64,8 +72,8 @@ function Shoe(props) {
         receiveShadow
         geometry={nodes.Bearded_Man_2.geometry}
         material={nodes.Bearded_Man_2.material}
-        position={[-0.31, 1.524, 0.11]}
-        rotation={[-1.93, 0.03, -0.14]}
+        position={[-1.20, 3.5, 0]}
+        rotation={[-1.83, 0.03, -0.14]}
         
       />
     </group>
@@ -94,14 +102,14 @@ export default function App() {
 
     
   </div>
-      <Canvas concurrent pixelRatio={[1, 1.5]} angle={0.31} camera={{ position: [-1 , 8, -8] }}>
+      <Canvas concurrent pixelRatio={[1, 1.5]}  camera={{ fov: 60, position: [1 , 0, -9] }}>
         
         <ambientLight intensity={0.3} />
         <spotLight intensity={1.3} angle={0.21} penumbra={1} position={[10, 10, 20]} />
         <Suspense fallback={<Loader />}>
           <Shoe />
           <Environment files="royal_esplanade_1k.hdr" />
-          <ContactShadows rotation-x={Math.PI / 2} position={[0, -0.8, 0]} opacity={0.25} width={1} height={20} blur={2} far={1} />
+          <ContactShadows rotation-x={Math.PI / 2} position={[0, -0.9, 0]} opacity={0.55} width={4} height={20} blur={2} far={1} />
         </Suspense>
         <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={true} enablePan={true} />
       </Canvas>
